fe326626bb1594bae666d9938c157242dccd2ced
Francis Sanchez <francis.albert.2009@gmail.com>
2025-01-29 21:28:17 -0800

    Install bootstrap and implemented functions in roles.component.ts

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-roles',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
-export class RolesComponent {
 
+export class RolesComponent {
+  firstName: string = 'Francis Sanchez';
+  angularVersion: string = 'Version 19';
+ 
+  version: number = 19.1;
+  isActive: boolean = false;
+  currentDate: Date = new Date();
+  inputType: string = 'checkbox';
+  selectedState: string = ''
+
+  showWelcomeAlert () {
+    alert('Welcome to Angular 19');
+  }
+
+  showMessage(message: string) {
+    alert(message)
+  }
 }
