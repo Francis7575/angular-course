9bb18907479450bdcfc9e705f13421276fc91acf
Francis Sanchez <francis.albert.2009@gmail.com>
2025-02-01 12:34:00 -0800

    configure app.route.ts, adding loading indicator when fetching the data from the API, utilizing Location module in order to indicate active tab on the menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MasterComponent } from './components/master/master.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MasterComponent],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-
 export class AppComponent {
-  title = 'angular-course';
+  constructor(private location: Location) {}
+
+  isActiveRoute(route: string): boolean {
+    return this.location.path() === route;
+  }
 }
--- src/app/components/designation/designation.component.ts
+++ src/app/components/designation/designation.component.ts
@@ -1,25 +1,28 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MasterService } from '../../service/master.service';
 import { APIResponseModel, IDesignation } from '../../model/interface/role';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-designation',
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './designation.component.html',
   styleUrl: './designation.component.css',
 })
+
 export class DesignationComponent implements OnInit {
   designationList: IDesignation[] = [];
   masterService = inject(MasterService);
+  isLoading: boolean = true
 
   ngOnInit(): void {
     this.masterService.getDesignations().subscribe({
       next: (result: APIResponseModel) => {
         this.designationList = result.data;
+        this.isLoading = false
       },
       error: (error) => {
         alert('API error/ Network Down');
+        this.isLoading = false
       },
     });
   }
