12dc4a0612506a2a74895225780f17d7c059e8c3
Francis Sanchez <francis.albert.2009@gmail.com>
2025-01-31 23:24:02 -0800

    Created service and succesfully fetched alldesignations and show it on the UI

--- src/app/components/designation/designation.component.ts
+++ src/app/components/designation/designation.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MasterService } from '../../service/master.service';
+import { APIResponseModel, IDesignation } from '../../model/interface/role';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-designation',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './designation.component.html',
-  styleUrl: './designation.component.css'
+  styleUrl: './designation.component.css',
 })
-export class DesignationComponent {
+export class DesignationComponent implements OnInit {
+  designationList: IDesignation[] = [];
+  masterService = inject(MasterService);
 
+  ngOnInit(): void {
+    this.masterService.getDesignations().subscribe({
+      next: (result: APIResponseModel) => {
+        this.designationList = result.data;
+      },
+      error: (error) => {
+        alert('API error/ Network Down');
+      },
+    });
+  }
 }
