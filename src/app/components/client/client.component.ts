9a40f58cfc06960cefb11e8001862d1f9425840c
Francis Sanchez <francis.albert.2009@gmail.com>
2025-02-03 17:20:58 -0800

    created Client class and store initial for it's respective property, succesfully getAllClients and AddUpdateClient from the API.

--- src/app/components/client/client.component.ts
+++ src/app/components/client/client.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { APIResponseModel } from './../../model/interface/role';
+import { ClientService } from './../../service/client.service';
+import { Component, OnInit, inject } from '@angular/core';
+import { Client } from '../../model/class/Client';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-client',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './client.component.html',
-  styleUrl: './client.component.css'
+  styleUrl: './client.component.css',
 })
-export class ClientComponent {
+export class ClientComponent implements OnInit {
+  clientObj: Client = new Client();
+  clientList: Client[] = [];
+  clientService = inject(ClientService);
 
+  ngOnInit(): void {
+    this.loadClient();
+  }
+
+  loadClient() {
+    this.clientService.getAllClients().subscribe((res: APIResponseModel) => {
+      this.clientList = res.data;
+    });
+  }
+
+  onSaveClient() {
+    console.log(this.clientObj); 
+    this.clientService
+      .addUpdate(this.clientObj)
+      .subscribe((res: APIResponseModel) => {
+        if (res.result) {
+          alert('Succesfully created!');
+        } else {
+          alert(res.message);
+        }
+      });
+  }
 }
